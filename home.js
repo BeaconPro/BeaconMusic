cd3a228fd9692f6721f42ba86bdcb2879805f723
Meisacoder123 <henryfboughan@gmail.com>
2025-04-16 19:43:11 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,11 @@
-function changeBackground() {
-    document.body.style.backgroundColor = "darkblue";
-    document.body.style.backgroundColor = "white";
+function darkMode() {
+    if (document.body.style.backgroundColor==="#74e1ff") {
+        document.body.style.backgroundColor = "darkblue";
+        document.body.style.color = "white";
+    }
+    else {
+        document.body.style.backgroundColor = "#74e1ff";
+        document.body.style.color = "black";
+    }
+  }
 }
